5f68a1a9114b98318501eab3e28f31d072402929
Adrián <adrian.pisabarro.garcia@gmail.com>
2019-11-06 01:09:02 +0100

    6 nov 2019

--- tema-2/workspace-practica-1/Ejercicio3/src/Ejercicio3.java
+++ tema-2/workspace-practica-1/Ejercicio3/src/Ejercicio3.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		Integer numIntroducido;
+		int numIntroducido;
 		char[] s;	
 		boolean bucle;
 		String cadena;
@@ -13,26 +13,32 @@ public class Ejercicio3 {
 		
 		s=new char[26];
 		for ( int i=0; i<26; i++) {
-			s[i] = (char) ('A' + i );
+			s[i] = (char) ('A' + i);
 		}
-		try {
+		
 			do {
-				numIntroducido = Integer.parseInt(JOptionPane.showInputDialog(null, "Inserta un número para que te salga la letra correspondiente a ese número"));
-				if (numIntroducido == -1) {
-					bucle = false;
-				}if (numIntroducido<-1 || numIntroducido>25) {
-					throw new teHasSalido();
-				}else {
-					JOptionPane.showMessageDialog(null, "La letra correspondiente es: "+s[numIntroducido]);
-					cadena = cadena + s[numIntroducido];
-				}
-			} while (bucle != false);
-			JOptionPane.showMessageDialog(null, "Cadena unida: "+cadena);
-		} catch (teHasSalido e) {
-			JOptionPane.showMessageDialog(null, "No has introducido un valor correcto entre -1 y 26");
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, "PROBLEMAS");
-		}
+				try {
+						numIntroducido = Integer.parseInt(JOptionPane.showInputDialog(null, "Inserta un número para que te salga la letra correspondiente a ese número"));
+						if (numIntroducido == -1) {
+							bucle = false;
+						}if (numIntroducido<-1 || numIntroducido>25) {
+							throw new teHasSalido();
+						}if (numIntroducido>-1 && numIntroducido <26) {
+							JOptionPane.showMessageDialog(null, "La letra correspondiente es: "+s[numIntroducido]);
+							cadena = cadena + s[numIntroducido];
+						}
+						
+					} catch (teHasSalido e) {
+						JOptionPane.showMessageDialog(null, "No has introducido un valor correcto entre -1 y 25");
+					} catch (Exception e) {
+						JOptionPane.showMessageDialog(null, "PROBLEMAS");
+					}
+				
+				} while (bucle != false);
+				JOptionPane.showMessageDialog(null, "Cadena unida: "+cadena);
+			
+			
+		
 		
 	}
 

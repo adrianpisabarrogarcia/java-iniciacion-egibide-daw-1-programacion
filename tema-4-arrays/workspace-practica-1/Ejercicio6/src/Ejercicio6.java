71e32cbf7e9b8c0613d5d292fdb1a9d117095b0c
Adrián <adrian.pisabarro.garcia@gmail.com>
2019-11-14 13:19:59 +0100

    20191114

--- tema-4-arrays/workspace-practica-1/Ejercicio6/src/Ejercicio6.java
+++ tema-4-arrays/workspace-practica-1/Ejercicio6/src/Ejercicio6.java
@@ -1,9 +1,18 @@
 import javax.swing.*;
-
 public class Ejercicio6 {
 
 	public static void main(String[] args) {
 		
+		String texto = JOptionPane.showInputDialog("Introducir texto");
+		String textoMinusuclas = texto;
+		char [] cadena = new char [texto.length()];
+		for (int i = 0; i < cadena.length; i++) {
+			char letra = textoMinusuclas.charAt(i);
+			cadena [i]= textoMinusuclas.charAt(i);
+		}
+		for (int i = 0; i < args.length; i++) {
+			
+		}
 		
 		
 	}

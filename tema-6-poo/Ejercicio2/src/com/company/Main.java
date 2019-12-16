1e491d108dda596ab4231c767f7196f7fbb5374a
Adrián PG <adrian.pisabarro.garcia@gmail.com>
2019-12-16 13:33:10 +0100

    16 dic 2019

--- tema-6-poo/Ejercicio2/src/com/company/Main.java
+++ tema-6-poo/Ejercicio2/src/com/company/Main.java
@@ -1,11 +1,13 @@
 package com.company;
 import java.util.ArrayList;
 import java.util.List;
+import MisClases.*;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        List<List<String>> listaEmpresaA = new ArrayList<List<String>>();
+
 
 
 
